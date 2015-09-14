2edd52ed6af816096f0e52ee3aa3bc97ec798f90
Dommme <dominik.meissner@informatik.hs-fulda.de>
2015-09-14 15:44:08 +0200

    Erste Spielerein, Singleplayer

---
+++ Gummibande/public_html/js/game.js
@@ -0,0 +1,143 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var THREE;
+var renderer, camera, scene;
+var actionStart, actionStop;
+var size, growth, rspeed, mspeed;
+var rollr, rolll, moveu, moved, mover, movel, reset;
+var glob, ball, globsize, ballsize;
+var ballposition, globposition, abstand;
+var score;
+var text2, text1;
+var currentscale;
+
+//
+//Ball wachsen Lassen
+//
+var wachsen = function(x) {
+            ballsize *= x;
+            currentscale *= x;
+            ball.scale.set( currentscale, currentscale, currentscale);
+};
+
+//
+//Glob neu positionieren
+//
+var moveGlob = function() {
+    glob.position.x = Math.random()*11 - 5.5;
+    glob.position.y = Math.random()*7 - 3.5;
+    text2.innerHTML = "Score: " + score;
+//    text1.innerHTML = "distanceToPoint: " + abstand.distance();
+    score += 1;
+};
+
+//
+//Abstand Prüfen
+//
+var collect = function(){
+    if (abstand.distance() < (ballsize+globsize)){
+        moveGlob();
+        wachsen(1.1);
+    }
+};
+
+//
+//Positionen Aktualisieren / Linie ziehen (nur nach Bewegung)
+//
+var positionSet = function(){
+    
+    ballposition.set(ball.position.x, ball.position.y, 0); 
+    globposition.set(glob.position.x, glob.position.y, 0);
+    abstand.set(ballposition, globposition);
+};
+
+//
+//Nach dem Rand wiederholen
+//
+var repeatBorder = function(){
+    
+    if (ball.position.x > 5.8){
+        ball.position.x = -5.7;}
+    if (ball.position.x < -5.8){
+        ball.position.x = 5.7;}
+    if (ball.position.y > 3.9){
+        ball.position.y = -3.8;}
+    if (ball.position.y < -3.9){
+        ball.position.y = 3.8;}
+};
+
+//
+//Bewegung in rotierte Richtung und Rotation
+//
+var move = function(){
+
+//Bewegung
+    if (moveu) {
+        ball.position.x += Math.sin(-(ball.rotation.z)) * mspeed;
+        ball.position.y += Math.cos(-(ball.rotation.z)) * mspeed;
+    }
+
+    if (moved) {
+        ball.position.x -= Math.sin(-(ball.rotation.z)) * mspeed;
+        ball.position.y -= Math.cos(-(ball.rotation.z)) * mspeed;
+    }
+
+    if (movel) {
+        ball.position.x -= Math.cos((ball.rotation.z)) * mspeed;
+        ball.position.y -= Math.sin((ball.rotation.z)) * mspeed;
+    }
+
+    if (mover) {
+        ball.position.x += Math.cos((ball.rotation.z)) * mspeed;
+        ball.position.y += Math.sin((ball.rotation.z)) * mspeed;
+    }
+    
+//Rotation
+    if (rollr === true) {
+        ball.rotation.z -= rspeed;
+        ball.rotation.z %= 6.28;}
+
+    if (rolll === true) {
+        ball.rotation.z += rspeed;
+        ball.rotation.z %= 6.28;}
+
+//Reset
+    if (reset) {
+        ball.rotation.z = 0;
+        ball.position.x = 0;
+        ball.position.y = 0;
+        ball.scale.set( 1, 1, 1);
+    }
+};
+
+
+//#######//
+//Rendern//
+//#######//
+var render = function () {
+    requestAnimationFrame( render );
+    
+    text1.innerHTML = ballsize + " + " + globsize + " = " + (ballsize+globsize) +  " . . . " + abstand.distance();
+    
+    move();
+    
+    repeatBorder();
+    
+    positionSet();    
+        
+    collect();
+
+    renderer.render(scene, camera);
+    
+    
+///////////
+//    glob.position.x = 5.8;
+//    glob.position.y = -3.9;
+//    ball.position.x = 5;
+//    ball.position.y += 0.1;
+    
+    
+};
---
+++ Gummibande/public_html/js/scene.js
@@ -0,0 +1,49 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, width/height, 0.1, 1000 );
+//    var camera = new THREE.OrthographicCamera( width / - 80, width / 80, height / 80, height / - 80, 1, 1000 );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.SphereGeometry( ballsize );
+    var geometry2 = new THREE.SphereGeometry( globsize );
+    var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false} );
+    var material2 = new THREE.MeshBasicMaterial( {color: 0x00ff00}) ;
+    
+    var ball = new THREE.Mesh( geometry, material );
+    var glob = new THREE.Mesh( geometry2, material2 );
+     
+    scene.add( ball );
+    scene.add( glob );
+
+    camera.position.z = 5; 
+    
+    var text1 = document.createElement('div');
+    text1.style.position = 'absolute';
+    text1.style.width = 500;
+    text1.style.height = 20;
+    text1.style.backgroundColor = "grey";
+    text1.innerHTML = "distanceToPoint: ?";
+    text1.style.top = 60 + 'px';
+    text1.style.left = 20 + 'px';
+    document.body.appendChild(text1);
+    
+    var text2 = document.createElement('div');
+    text2.style.position = 'absolute';
+    text2.style.width = 60;
+    text2.style.height = 20;
+    text2.style.backgroundColor = "grey";
+    text2.innerHTML = "Score: 0";
+    text2.style.top = 80 + 'px';
+    text2.style.left = (width/2 + 50) + 'px';
+    document.body.appendChild(text2);
+    
+    document.addEventListener("keydown", actionStart, false);
+    document.addEventListener("keyup", actionStop, false);
+    
