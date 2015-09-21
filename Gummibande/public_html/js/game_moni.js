5b0be455aeab54a3bf18ce705886790116288c86
Moni1992 <monja.heneka@informatik.hs-fulda.de>
2015-09-21 12:45:51 +0200

    Modell Gestaltung
    Moni Game

--- Gummibande/public_html/js/game_moni.js
+++ Gummibande/public_html/js/game_moni.js
@@ -4,4 +4,15 @@
  * and open the template in the editor.
  */
 
+    var geometry1 = new THREE.SphereGeometry( ballsize );
+    var geometry2 = new THREE.SphereGeometry( globsize );
+    var material1 = new THREE.MeshBasicMaterial( {color: 0xff0000});
+    var material2 = new THREE.MeshBasicMaterial( {color: 0x00ff00});
+    
+    var ball = new THREE.Mesh( geometry1, material1 );
+    var glob = new THREE.Mesh( geometry2, material2 );
+     
+    scene.add( ball );
+    scene.add( glob );
+
 
