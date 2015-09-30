533e91d0887e0a3d4de203e363242ab295d61889
Dommme <dominik.meissner@informatik.hs-fulda.de>
2015-09-30 13:32:25 +0200

    bilder hinzugefügt

--- Gummibande/public_html/js/game_dome.js
+++ Gummibande/public_html/js/game_dome.js
@@ -6,6 +6,10 @@
 
 var fieldwidth, fieldheight, THREE, scene, pukposition, pukabstand, pp1, pp2, obj, belongs, pukgo;
 
+var fieldgeo = new THREE.PlaneGeometry(17, 11);
+var fieldmap = new THREE.ImageUtils.loadTexture("files/field.png");
+var fieldmat = new THREE.MeshBasicMaterial({map:fieldmap});
+
 var pukgeometry = new THREE.SphereGeometry(0.5, 8, 8);
 //var pukmaterial = new THREE.MeshBasicMaterial({color: 0x0000ff});
 var spritemap = THREE.ImageUtils.loadTexture("files/ball.jpg");
@@ -15,28 +19,32 @@ var pukmaterial = new THREE.MeshBasicMaterial({map: spritemap});
 var p1geo = new THREE.BoxGeometry(0.35, 2,0.1);
 //var p1mat = new THREE.MeshBasicMaterial({color: 0x00ff00});
 var player1map = new THREE.ImageUtils.loadTexture("files/player1.png");
-var p1mat = new THREE.MeshBasicMaterial ({map:player1map});
+var p1mat = new THREE.MeshBasicMaterial ({map:player1map, transparent: true});
 var p2geo = new THREE.BoxGeometry(0.35, 2,0.1);
 //var p2mat = new THREE.MeshBasicMaterial({color: 0xff0000});
 var player2map = new THREE.ImageUtils.loadTexture("files/player2.png");
-var p2mat = new THREE.MeshBasicMaterial ({map:player2map});
+var p2mat = new THREE.MeshBasicMaterial ({map:player2map, transparent: true});
 var ppgeo = new THREE.SphereGeometry(0.20);
 var ppmat = new THREE.MeshBasicMaterial({color: 0x00ffff});
 
-var beargeo = new THREE.BoxGeometry(0.5);
+var beargeo = new THREE.BoxGeometry(1, 1.6, 0.1);
 var bearmap = new THREE.ImageUtils.loadTexture("files/bear.png");
 var bearmat = new THREE.MeshBasicMaterial({map:bearmap});
+bearmat.transparent = true;
 var bear1 = new THREE.Mesh(beargeo, bearmat);
 var bear2 = new THREE.Mesh(beargeo, bearmat);
 
 var puk = new THREE.Mesh(pukgeometry, pukmaterial);
-puk.rotation.z = Math.random()*1.5+1;
+//puk.rotation.z = Math.random()*1.5+1;
+puk.rotation.z = 0.87;
 var p1 = new THREE.Mesh(p1geo, p1mat);
 var p2 = new THREE.Mesh(p2geo, p2mat);
+var field = new THREE.Mesh(fieldgeo, fieldmat);
 
 function loadGameFour(){
 gamedome = true;
 scene.add(puk);
+scene.add(field);
 
 //for (var i = -1; i <= 1; i += 0.4){
 //    var pp1 = new THREE.Mesh(ppgeo, ppmat);
@@ -46,11 +54,12 @@ scene.add(puk);
 //    p1.add(pp1);
 //    p2.add(pp2);
 //}
-bear1.position.set(-1, 0, 0);
+bear1.position.set(-0.4, 0, 0);
 p1.add(bear1);
 p1.position.set(-fieldwidth+1, 0, 0);
 scene.add(p1);
-bear2.position.set(+1, 0, 0);
+bear2.rotation.z = 3.1415;
+bear2.position.set(+0.4, 0, 0);
 p2.add(bear2);
 p2.position.set(fieldwidth-1, 0, 0);
 scene.add(p2);
@@ -129,12 +138,12 @@ function checkPlayers(){
     if(belongs){
         if(puk.position.x < -fieldwidth+1.4 && puk.position.x > -fieldwidth+1){
             if(puk.position.y < p1.position.y+1.3 && puk.position.y > p1.position.y){
-                puk.rotation.z = -(puk.rotation.z)+0.2;
+                puk.rotation.z = -(puk.rotation.z)+0.05;
                 belongs = false;
                 pukspeed *= 1.05;
             }
             if(puk.position.y > p1.position.y-1.3 && puk.position.y < p1.position.y){
-                puk.rotation.z = -(puk.rotation.z)-0.2;
+                puk.rotation.z = -(puk.rotation.z)-0.05;
                 belongs = false;
                 pukspeed *= 1.05;
             }
@@ -143,12 +152,12 @@ function checkPlayers(){
     else{
         if(puk.position.x < fieldwidth-1 && puk.position.x > fieldwidth-1.4){
             if(puk.position.y < p2.position.y+1.3 && puk.position.y > p2.position.y){
-                puk.rotation.z = -(puk.rotation.z)-0.2;
+                puk.rotation.z = -(puk.rotation.z)-0.05;
                 belongs = true;
                 pukspeed *= 1.05;
             }
             if(puk.position.y > p2.position.y-1.3 && puk.position.y < p2.position.y){
-                puk.rotation.z = -(puk.rotation.z)+0.2;
+                puk.rotation.z = -(puk.rotation.z)+0.05;
                 belongs = true;
                 pukspeed *= 1.05;
             }
