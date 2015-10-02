ea21da924dc3a66f67d13edda6ecb176461341bb
Moni1992 <monja.heneka@informatik.hs-fulda.de>
2015-10-02 14:57:53 +0200

    Minus oder Plus, hoffentlich ist jetzt Schluss

--- Gummibande/public_html/js/constants.js
+++ Gummibande/public_html/js/constants.js
@@ -5,8 +5,8 @@
  */
 
 
-var width = window.innerWidth - 250;
-var height = window.innerHeight -250;
+var width = window.innerWidth + 100;
+var height = window.innerHeight + 100;
 
 var FONT = "bold 20px sans-serif";
 var posx = 100;
