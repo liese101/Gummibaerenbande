001781f2f42fa8d1726e69c6229460e5cdaafd95
liese101 <claudia.toebben@informatik.hs-fulda.de>
2015-10-02 22:30:58 +0200

    hintergrundsound auskommentiert

--- Gummibande/public_html/js/game_dome.js
+++ Gummibande/public_html/js/game_dome.js
@@ -113,15 +113,15 @@ console.log("Audiolistener added");
 //audio
 
 //audiovariablen
-var hintergund_sound_d;
+//var hintergund_sound_d;
 var bande_sound;
 var aus_sound;
 var spieler_sound;
 
-hintergrund_sound_d = new THREE.Audio(listener);
-hintergrund_sound_d.load("files/frosch im gras.ogg");
-hintergrund_sound_d.autoplay = true;
-hintergrund_sound_d.setLoop(true);
+//hintergrund_sound_d = new THREE.Audio(listener);
+//hintergrund_sound_d.load("files/frosch im gras.ogg");
+//hintergrund_sound_d.autoplay = true;
+//hintergrund_sound_d.setLoop(true);
 
 //kontakt mit Spielern prüfen
 function checkPlayers(){
