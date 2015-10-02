4c4c4928bfc94884ed5366ae5ddb699b2d5c38c3
Moni1992 <monja.heneka@informatik.hs-fulda.de>
2015-10-02 21:57:04 +0200

    test

--- Gummibande/public_html/js/game_moni.js
+++ Gummibande/public_html/js/game_moni.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Alle Variablen werden zur Fehlerbehebung geladen
 var THREE;
 var renderer, camera, scene;
 var actionStart, actionStop;
